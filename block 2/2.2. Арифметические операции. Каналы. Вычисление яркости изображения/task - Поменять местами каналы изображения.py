5e73f8d213e6955317aada9d27b628301a23c02b
andrew k <113019270+kdduha@users.noreply.github.com>
2023-08-12 22:54:36 +0500

    добавил комментарии к коду

--- block 2/2.2. Арифметические операции. Каналы. Вычисление яркости изображения/task - Поменять местами каналы изображения.py
+++ block 2/2.2. Арифметические операции. Каналы. Вычисление яркости изображения/task - Поменять местами каналы изображения.py
@@ -2,5 +2,7 @@ from skimage.io import imread, imsave
 import numpy as np
 
 img = imread('img.png')
+# сохраняем по отдельности каналы как слайсы третьего измерения 3-ех мерной матрицы
 r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+# все заносим в матрицу с новым порядком (будет опять 3-ех мерный массив)
 imsave('out_img.png', np.dstack((b, r, g)))
