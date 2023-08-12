58896cf09e9a84a4db590ab9a8145f8af33b5191
andrew k <113019270+kdduha@users.noreply.github.com>
2023-08-12 22:56:26 +0500

    добавил комментарии к коду

--- block 2/2.2. Арифметические операции. Каналы. Вычисление яркости изображения/task - Подсчет яркости изображения.py
+++ block 2/2.2. Арифметические операции. Каналы. Вычисление яркости изображения/task - Подсчет яркости изображения.py
@@ -3,4 +3,5 @@ from skimage import img_as_float, img_as_ubyte
 
 img = img_as_float(imread('img.png'))
 r, g, b = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+# считаем яркость по формуле поэлементно (каждый канал - 2-мерный numpy массив, так что так можно)
 imsave('out_img.png', img_as_ubyte(0.2126*r + 0.7152*g + 0.0722*b))
