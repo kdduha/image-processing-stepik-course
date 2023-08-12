b80480762ff03f6317bf590854e6ebec75582d23
andrew k <113019270+kdduha@users.noreply.github.com>
2023-08-12 22:52:55 +0500

    добавил комментарии к коду

--- block 2/2.2. Арифметические операции. Каналы. Вычисление яркости изображения/task - Вычисление негатива изображения.py
+++ block 2/2.2. Арифметические операции. Каналы. Вычисление яркости изображения/task - Вычисление негатива изображения.py
@@ -1,4 +1,7 @@
 from skimage.io import imread, imsave
 from skimage import img_as_float
 
+# переводим во float для удобства (можно и с int)
+# инвертируем значения поэлементно (numpy массивы так умеют), вычитывая -1 из всех чисел из диапазона [0; 1]
+# избавляемся от минуса по модулю
 imsave('out_img.png', abs(img_as_float(imread('img.png')) - 1))
