c1adf16810b6ff65c5331b514af78ab5ba71f194
andrew k <113019270+kdduha@users.noreply.github.com>
2023-08-12 23:00:39 +0500

    добавил комментарии к коду

--- block 3/3.1. Линейная коррекция контраста/task - Автоконтраст черно-белого изображения.py
+++ block 3/3.1. Линейная коррекция контраста/task - Автоконтраст черно-белого изображения.py
@@ -1,5 +1,11 @@
 from skimage.io import imshow, imread, imsave
+
 img = imread('img.png')
+
+# вычленяю мин. и макс. значение из одномерного массива = матрица изображения, расстянутая в один ряд
+# хотя можно все сделать и с помощью np.min/np.max
 img_min, img_max = min(img.ravel()), max(img.ravel())
+
+# подставляю в формулу
 img = (img - img_min) * (255 / (img_max - img_min))
 imsave('out_img.png', img.astype('uint8'))
