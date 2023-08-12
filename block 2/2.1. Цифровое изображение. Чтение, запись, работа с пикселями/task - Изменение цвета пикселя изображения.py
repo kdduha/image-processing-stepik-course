95018663a1224f8d4a723a26ea4044228d9a9f02
andrew k <113019270+kdduha@users.noreply.github.com>
2023-08-12 22:42:33 +0500

    добавил комментарии к коду

--- block 2/2.1. Цифровое изображение. Чтение, запись, работа с пикселями/task - Изменение цвета пикселя изображения.py
+++ block 2/2.1. Цифровое изображение. Чтение, запись, работа с пикселями/task - Изменение цвета пикселя изображения.py
@@ -1,4 +1,6 @@
 from skimage.io import imread, imshow, imsave
+
 img = imread('img.png')
+# центр находим за счет целочисленного деления пополам n-ой строки и m-ого столбца
 img[img.shape[0]//2, img.shape[1]//2] = [102, 204, 102]
 imsave('out_img.png', img)
