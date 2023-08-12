0c831884bcd1c502b10259a9538e58803d20c8d5
andrew k <113019270+kdduha@users.noreply.github.com>
2023-08-12 23:11:07 +0500

    добавил комментарии к коду

--- block 3/3.1. Линейная коррекция контраста/task - Устойчивый автоконтраст черно-белого изображения.py
+++ block 3/3.1. Линейная коррекция контраста/task - Устойчивый автоконтраст черно-белого изображения.py
@@ -1,9 +1,11 @@
 from skimage.io import imread, imsave
 import numpy as np
 
-img = imread('img.png')
+# объединяю методы из предыдущих двух задач
+img = imread('img.png').astype('float')
 k, h = round(img.size * 0.05), sorted(img.ravel())
 min_pix, max_pix = h[k], h[-k-1]
 
-img = (np.clip(img, min_pix, max_pix) - min_pix)*(255/(max_pix - min_pix))
-imsave('out_img.png', img.astype('uint8'))
+# иду по формуле + инструкции задачи
+img = (img - min_pix) * (255 / (max_pix - min_pix))
+imsave('out_img.png', np.clip(img, min_pix, max_pix).astype('uint8'))
